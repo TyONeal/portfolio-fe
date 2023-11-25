6171bf7e9dd57f6c7b5e9b71d6ff69360d5f3bcf
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-24 20:23:44 -0600

    started navbar basis

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,7 +1,12 @@
 function Navbar() {
     return (
         <nav>
-            Nav start
+            <ul>
+                <li>Projects</li>
+                <li>About Me</li>
+                <li>GitHub</li>
+                <li>LinkedIn</li>
+            </ul>
         </nav>
     )
 }
