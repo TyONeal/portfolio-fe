6769a9eb5562f04d3dff9f50d41b91099cb41413
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-27 22:38:15 -0600

    base setup for hero completed

--- src/components/hero/HeroDescription.js
+++ src/components/hero/HeroDescription.js
@@ -3,11 +3,11 @@ import "../../css/hero-description.css";
 function HeroDescription() {
     return (
         <div className="description-wrapper">
-            <div className="hero-image">
+            <div className="hero-row">
                 <div className="hero-name">Ty O'Neal</div>
-            </div>
-            <div className="hero-content">
-                Hero description content
+                <div className="hero-content">
+                    Ty O'Neal is an Air Force Vet turned programmer located in San Antonio, Texas. With over 15 years of experience in the workforce, his expertise is in managing solutions in high-stress (often crunch) times. This site is an extension of his skills in web development. It was made with React, with a Java (Spring Boot) backend.
+                </div>
             </div>
         </div>
     )
