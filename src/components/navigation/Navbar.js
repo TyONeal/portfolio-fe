7fd5505908c65915cf9d720f67654981be6ee768
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-27 21:39:51 -0600

    fixed scaling on navbar anchors by changing display to flex

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -4,10 +4,10 @@ function Navbar() {
     return (
         <nav className="nav-container">
             <ul>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/aboutMe">About Me</a></li>
-                <li><a href="https://github.com/TyONeal">GitHub</a></li>
-                <li><a href="https://www.linkedin.com/in/ty-oneal/">LinkedIn</a></li>
+                <li><a href="/projects" target="_blank" rel="noopener">Projects</a></li>
+                <li><a href="/aboutMe" target="_blank" rel="noopener">About Me</a></li>
+                <li><a href="https://github.com/TyONeal" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/ty-oneal/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             </ul>
         </nav>
     )
