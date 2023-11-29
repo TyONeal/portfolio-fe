20c8bb5c05c781f3b5eedd90392ccc73ce76e283
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-29 09:03:27 -0600

    started styling for project cards

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,8 +1,18 @@
-import "../../css/projects/project-card.css";
+import "../../css/projects/projects.css";
 
-function ProjectCard() {
+function ProjectCard(proj) {
     return (
-        <div>Hi</div>
+        <div className="card">
+            <div className="card-header">
+                Header
+            </div>
+            <div className="card-description">
+                Description
+            </div>
+            <div className="card-image">
+                <img src={proj.image} alt="project" />
+            </div>
+        </div>
     )
 }
 
--- src/components/projects/ProjectWrapper.js
+++ src/components/projects/ProjectWrapper.js
@@ -1,11 +1,18 @@
 import ProjectCard from "./ProjectCard";
 
+import "../../css/projects/projects.css";
+
 function ProjectWrapper({proj}) {
     return (
-        <div>
-            <ProjectCard project={proj} />
-            <ProjectCard project={proj} />
-            <ProjectCard project={proj} />
+        <div className="project-wrapper">
+            <div className="project-header">
+                My Projects
+            </div>
+            <div className="cards-wrapper">
+                <ProjectCard project={proj} />
+                <ProjectCard project={proj} />
+                <ProjectCard project={proj} />
+            </div>
         </div>
     )
 }
