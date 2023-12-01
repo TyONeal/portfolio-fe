530c83be28a4593477661375d89ed780d8970a60
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-01 00:26:39 -0600

    got basic structure of hero content on board. need to rework, but good start

--- src/components/hero/HeroDescription.js
+++ src/components/hero/HeroDescription.js
@@ -6,15 +6,22 @@ function HeroDescription() {
             <div className="hero-row">
                 <div className="hero-name">Ty O'Neal</div>
                 <div className="hero-content">
-                    <span>What I Prioritize as a Dev:</span>
-                    <ul className="value-list">
-                        <li>User Value</li>
-                        <br />
-                        <li>Code That Works</li>
-                        <li>Neat Code</li>
-                        <li>Good Turnover</li>
-                    </ul>
-                    <p>Hi there</p>
+                    <div className="hero-content-header">
+                       <span>What I Prioritize as a Dev:</span>
+                    </div>
+                    <div className="hero-content-list">
+                        <ul className="value-list">
+                            <li>User Value</li>
+                            <li>Code That Works</li>
+                            <li>Neat Code</li>
+                            <li>Good Turnover</li>
+                        </ul>
+                    </div>
+                    <div className="hero-content-mission">
+                        <p>
+                            Programming is, at its best, gray. Context should always be considered when building applications. Personally, I always lean on the side of UX first. We are creating technologies to assist (perhaps even delight) our customers. Before I ship code out, the first thing I'm looking at is: <i>does this code provide a service or convenience to my user-base?</i> If it doesn't, I tend to re-evaluate. That doesn't mean I write messy code. It just means at the top level, I'm here to build products for people to use.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
