34b55e457f94d1c67279e4c7cff99f510143bc36
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-12-10 14:21:41 -0600

    set up link to kyterescue

--- src/components/hero/HeroDescription.js
+++ src/components/hero/HeroDescription.js
@@ -1,12 +1,12 @@
-import "../../css/hero/hero-description.css";
+import "../../css/hero/hero.css";
 
 function HeroDescription() {
     return (
         <div className="description-wrapper">
             <div className="hero-row">
-                <div className="hero-name">Ty O'Neal</div>
+                <div className="hero-name"><i>Ty O'Neal</i></div>
                 <div className="projects">
-                    Kyte
+                    <a href="https://kyterescue.com" target="_blank"><i>Kyte Rescue</i></a>
                 </div>
             </div>
         </div>
